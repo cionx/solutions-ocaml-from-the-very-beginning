e4f343fd1a0d836baad3c93840437f4093cfc371
Jendrik Stelzner <jendrikstelzner@web.de>
2024-02-24 15:29:50 +0100

    Add a solution to question 5.3.

---
+++ 05-sorting-things/question-05.03.md
@@ -0,0 +1,7 @@
+# Question 5.3
+
+> Write a version of insertion sort which sorts the argument list into reverse order.
+
+---
+
+We only have to replace the condition `hx <= hy` in `merge` by `hx >= hy`.
---
+++ 05-sorting-things/question-05.03.ml
@@ -0,0 +1,43 @@
+(** From the book. *)
+
+let rec length l =
+  match l with
+  | [] -> 0
+  | _ :: t -> 1 + length t
+
+let rec take n l =
+  match l with
+  | [] -> []
+  | h :: t ->
+    if n = 0 then []
+    else h :: take (n - 1) t
+
+let rec drop n l =
+  match l with
+  | [] -> []
+  | _ :: t ->
+    if n = 0 then l
+    else drop (n - 1) t
+
+
+
+(** For this exercise. *)
+
+let rec merge x y =
+  match x, y with
+  | [], l -> l
+  | l, [] -> l
+  | hx :: tx, hy :: ty ->
+      if hx >= hy
+      then hx :: merge tx y
+      else hy :: merge x ty
+
+let rec msort l =
+  match l with
+  | [] -> []
+  | [x] -> [x]
+  | _ ->
+    let len = length l / 2 in
+    let left = take len l in
+    let right = drop len l in
+    merge (msort left) (msort right)
